b5f5c6b209b4e64fd81f01b202dfb9d9d717db87
Trevor Moses <mosest68@gmail.com>
2026-04-04 16:34:07 -0700

    Trying to put text into cpp

---
+++ week1/hello_git.cpp
@@ -0,0 +1,18 @@
+/*
+First Program:  Hello C++ my old friend
+Date Created:   20260404
+Author:         Trevor Moses
+Purpose:        To complete Code Assignment Zero.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    /*example from book.
+    cout << "Hello, World." << endl;
+    Is this like javascript where lines need to end in semi-colons?
+    Runeforge says end of a statement. what counts as a statement.*/
+    cout << "My first Git-tracked C++ program." << endl;
+    return 0;
+}
