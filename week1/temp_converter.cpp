9691478c01e642a3639b59c266e7b93880f3b710
Trevor Moses <mosest68@gmail.com>
2026-04-05 19:07:18 -0700

    A days work is never done, but this is pretty good.

--- week1/temp_converter.cpp
+++ week1/temp_converter.cpp
@@ -13,35 +13,37 @@ Farenheit to Celcius and vice versa.
 using namespace std;
 
 
+// Apparently, functions go outside of main and variables are not global.
+void TempConvert(char F_or_C, double temp_input) {
+    double convertted_temp;
+    switch(F_or_C) {
+        case 'F':  // char literals need single quotes
+            cout << "Farhenheit? No, you get Celcius." << endl;
+            convertted_temp = (temp_input - 32) * 5.0/9.0;
+            cout << "Your temperature is now: " << convertted_temp << " C" << endl;
+            break;
+        case 'C':  // char literals need single quotes
+            cout << "Celcius? No, you get Farhenheit." << endl;
+            convertted_temp = (temp_input * 9.0/5.0) + 32;  // Fixed: wrong formula was here
+            cout << "Your temperature is now: " << convertted_temp << " F" << endl;
+            break;
+        default:
+            cout << "You didn't enter what I wanted you to. The program is ruined!" << endl;
+            break;
+    }
+}
+
 int main() {
-    //Variable Section
+    // Variable Section
     char F_or_C;
     double temp_input;
 
-    //Function to convert entered temperature.
-    void TempConvert(temp_input) {
-        double convertted_temp;
-        switch(F_or_C) {
-            case F:
-                cout << "Farhenheit? No, you get Celcius." << endl;
-                convertted_temp = (temp_input - 32) * 5/9; 
-                break;
-            case C:
-                cout << "Celcius? No, you get Farhenheit." << endl;
-                convertted_temp = (temp_input - 32) * 5/9 ;
-                break;
-            default:
-                cout << "You didn't enter what I wanted you to. The program is ruined!" << endl;
-                break;
-        }
-    }
-
-    //Ask for user to input what kind of temp.
-    cout << "What you want? (F)arhenheit or (C)elcius?";
+    // Ask for user to input what kind of temp.
+    cout << "What you want? (F)arhenheit or (C)elcius? ";
     cin >> F_or_C;
     cin >> temp_input;
-    TempConvert(temp_input);
 
-
+    //what happens when the function and the main use the same variables? Do we really have to declare them twice?
+    TempConvert(F_or_C, temp_input);
     return 0;
 }
