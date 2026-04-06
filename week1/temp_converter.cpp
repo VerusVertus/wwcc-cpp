536fbcaf416b797d472c2af3b550b3801fd953b6
Trevor Moses <mosest68@gmail.com>
2026-04-06 16:46:54 -0700

    Made change to make sure the char is always uppercase. Fixed more spelling errors. Added spaces for input.

--- week1/temp_converter.cpp
+++ week1/temp_converter.cpp
@@ -2,30 +2,25 @@
 Program Name: Temperature Converter
 Date created: 20260405
 Author: Trevor Moses
-Purpose: To take inputted units and temperature reading and convert it to the other units. 
-Farenheit to Celcius and vice versa.
-
+Purpose: To take inputted units and temperature reading and convert it to the other units.
+Fahrenheit to Celsius and vice versa.
 */
-
 #include <iostream>
-#include <string>
-//#include <cmath> Don't think i need this.
+#include <cctype> //wanted the toupper. Acording to the internet I need this for it to work.
 using namespace std;
 
-
-// Apparently, functions go outside of main and variables are not global.
 void TempConvert(char F_or_C, double temp_input) {
-    double convertted_temp;
+    double converted_temp;
     switch(F_or_C) {
-        case 'F':  // char literals need single quotes
-            cout << "Farhenheit? No, you get Celcius." << endl;
-            convertted_temp = (temp_input - 32) * 5.0/9.0;
-            cout << "Your temperature is now: " << convertted_temp << " C" << endl;
+        case 'F':
+            cout << "Fahrenheit? No, you get Celsius." << endl; 
+            converted_temp = (temp_input - 32) * 5.0/9.0;
+            cout << "Your temperature is now: " << converted_temp << " C" << endl;
             break;
-        case 'C':  // char literals need single quotes
-            cout << "Celcius? No, you get Fahrenheit." << endl;
-            convertted_temp = (temp_input * 9.0/5.0) + 32;  // Fixed: wrong formula was here
-            cout << "Your temperature is now: " << convertted_temp << " F" << endl;
+        case 'C':
+            cout << "Celsius? No, you get Fahrenheit." << endl;
+            converted_temp = (temp_input * 9.0/5.0) + 32;
+            cout << "Your temperature is now: " << converted_temp << " F" << endl;
             break;
         default:
             cout << "You didn't enter what I wanted you to. The program is ruined!" << endl;
@@ -34,21 +29,16 @@ void TempConvert(char F_or_C, double temp_input) {
 }
 
 int main() {
-    // Variable Section
     char F_or_C;
     double temp_input;
 
-
-    cout << "What is the temperature you wish to convert?";
+    cout << "What is the temperature you wish to convert? ";
     cin >> temp_input;
 
-    // Ask for user to input what kind of temp.
-    cout << "Is this (F)ahrenheit or (C)elcius? ";
+    cout << "Is this (F)ahrenheit or (C)elsius? ";
     cin >> F_or_C;
+    F_or_C = toupper(F_or_C);
 
-
-
-    //what happens when the function and the main use the same variables? Do we really have to declare them twice?
     TempConvert(F_or_C, temp_input);
     return 0;
 }
