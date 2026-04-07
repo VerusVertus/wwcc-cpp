45ceeea4c404994c1ab948218b8f38e60714b568
Trevor Moses <mosest68@gmail.com>
2026-04-06 18:10:03 -0700

    Adding starter file. Will work on later.

---
+++ week2/format_specifiers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>   // For printf
+using namespace std;
+
+int main() {
+    int integer_value = 42;
+    float float_value = 3.14159265f;
+    double double_value = 3.14159265;
+    char char_value = 'X';
+    const char* string_value = "Hello, World!";
+
+    printf("Format Specifier Practice\n");
+    printf("------------------------\n\n");
+
+// Your code here - use printf with different format specifiers
+
+    return 0;
+}
