74fc8ae727a1eb02685c20b02eeb13fc29c4f5db
Trevor Moses <mosest68@gmail.com>
2026-04-12 14:59:09 -0700

    Creating program layout. Need to code rand still.

--- week2/coin_flip.cpp
+++ week2/coin_flip.cpp
@@ -1,5 +1,16 @@
 /*
-Author info here
+Program:        Coin Flip Generator
+Date Created:   20260404
+Author:         Trevor Moses
+Purpose:        To complete Code Assignment 201.
+
+Assignment Requirements
+
+    Ask the user how many times they want to flip the coin
+    Simulate flipping a coin that many times using random numbers
+    Count and display the number of heads and tails
+    Calculate and display the percentage of each outcome
+
 */
 
 #include <iostream>
@@ -13,12 +24,34 @@ int main() {
     int flips;
     int heads = 0;
     int tails = 0;
+    int flips_done = 0;
+    char result;
 
     cout << "Coin Flip Simulator" << endl;
     cout << "How many flips would you like to simulate? ";
     cin >> flips;
 
-    // Your code here
+  do-while (flips_done < flips))
+  {
+    //Figure our how to specific characters.
+    result = rand()
+    
+    switch (result))
+    {
+    case h:
+        /* code */
+        break;
+    case t:
+        /* code */
+        break;
+    }
+    
+  }
+  
+  //Here we put the text ourside the switch statement.
+  cout << "heads" << heads << endl;
+  count << "tails" << tails << endl;
+
 
     return 0;
 }
