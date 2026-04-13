84d1c573f3d32095fcd31fcdc15323421085ac95
Trevor Moses <mosest68@gmail.com>
2026-04-12 18:12:05 -0700

    Cleaning up code. Moving notes to dedicated section.

--- week2/coin_flip.cpp
+++ week2/coin_flip.cpp
@@ -3,14 +3,6 @@ Program:        Coin Flip Generator
 Date Created:   20260404
 Author:         Trevor Moses
 Purpose:        To complete Code Assignment 201.
-
-Assignment Requirements
-
-    Ask the user how many times they want to flip the coin
-    Simulate flipping a coin that many times using random numbers
-    Count and display the number of heads and tails
-    Calculate and display the percentage of each outcome
-
 */
 
 #include <iostream>
@@ -21,13 +13,6 @@ using namespace std;
 int main() {
     srand(time(0));
 
-    /*
-    --Putting this in an array--
-    int flips;
-    int heads = 0;
-    int tails = 0;
-    int flips_done = 0;
-    */
     //[0] = numFlips, [1] = flips_done, [2] = heads, [3] = tails
     int coinStats[4] = {0,0,0,0};
     char result;
@@ -38,31 +23,20 @@ int main() {
     cout << "Simulating " << coinStats[0] << " flips..." << endl;
 
   do  {
-    /*    
-    condition ? valueIfTrue : valueIfFalse
-    int max = (a > b) ? a : b;  // max will be 20
-    */
 
-    /*
-    Hint provided: rand() % 2
-    cplusplus.com/reference/cstdlib/rand/
-    plusplus.com/doc/tutorial/operators/
-    Turn it into a true false and then assign character to it! what a work around.
-    */
+        result = rand() % 2 == 0 ? 'h' : 't';
 
-    result = rand() % 2 == 0 ? 'h' : 't';
-
-    switch (result)
-    {
-    case 'h':
-        coinStats[1]++;
-        coinStats[2]++;
-        break;
-    case 't':
-        coinStats[1]++;
-        coinStats[3]++;
-        break;
-    }
+        switch (result)
+        {
+        case 'h':
+            coinStats[1]++;
+            coinStats[2]++;
+            break;
+        case 't':
+            coinStats[1]++;
+            coinStats[3]++;
+            break;
+        }
   } while (coinStats[1] < coinStats[0]);
   
   cout << "Heads: " << coinStats[2] << "(" << coinStats[2]*100/coinStats[0] << "%)" <<  endl;
@@ -71,3 +45,11 @@ int main() {
 
     return 0;
 }
+
+    /*
+    NOTES SECTION:
+    Hint provided: rand() % 2
+    cplusplus.com/reference/cstdlib/rand/
+    plusplus.com/doc/tutorial/operators/
+    Turn it into a true false and then assign character to it! what a work around.
+    */
