d661685fe5d7e19b7402f32627a07d36a9e7b25e
Trevor Moses <mosest68@gmail.com>
2026-04-17 17:45:19 -0700

    Fixing duplication. adding responses to various attempts. Finishing loop.

--- week2/number_guessing.cpp
+++ week2/number_guessing.cpp
@@ -26,19 +26,57 @@ int main() {
     int guess;
     int attempts = 0;
 
+
+    //Responses Section. 
+    string tooLow[] = { "Were you looking for the ground? Well, you need to go higher\n",
+                        "Icarus aimed for the sun, you should look further up too\n",
+                        "Wrong. Are you trying to match your high schools GPA? Increase your guess\n",
+                        "We just restocked. You need to increase that number.\n"
+                    };
+    string tooHigh[] = { "They say their is not mountain high enough. Well, you might want to go lower\n",
+                         "You went high, well now you should go low(er)\n",
+                         "We are are above the clouds! You should look to the descent. Choose a smaller number\n",
+                         "I'll be frank, your number is going in the wrong direction. Reverse course.\n"
+                    };
+
+    string justRight[] = { "Wow-zer! You got it! The number was ",
+                           "Are you sure you don't have telepathy? The number was indeed ",
+                           "Bullseye! High Score! You're on target. The number was ",
+                           "Your answer, for a million secret points.....is CORRECT! The number was "
+
+                    };
+
+    int tooLowSize = std::size(tooLow);
+    int tooHighSize = std::size(tooHigh);
+    int justRightSize = std::size(justRight);
+
     cout << "Number Guessing Game\n";
     cout << "I'm thinking of a number between 1 and 100.\n\n";
 
-    do {
-        cout << "Number Guessing Game\nI'm thinking of a number between 1 and 100.\n\nEnter your guess: ";
-        if (!(cin >> coinStats[0])) {
-            cin.clear();                // clear error flag
-            cin.ignore(10000, '\n');    // discard bad input
-            coinStats[0] = -1;         // force back into loop
+ do {
+        cout << "Enter your guess: ";
+
+        if (!(cin >> guess)) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Invalid input. Please enter a number between 1 and 100.\n\n";
+            continue;
         }
-        if (coinStats[0] <= 0 || coinStats[0] > 1000000000)
-            cout << "Please enter a number between 1 and 1,000,000,000\n\n[------------Restarting------------]\n\n";
+
+        if (guess < 1 || guess > 100) {
+            cout << "Please enter a number between 1 and 100.\n\n";
+            continue;
+        }
+
+        attempts++;
+
+        if (guess < secretNumber)
+            cout << "Too low! Try again.\n\n";
+        else if (guess > secretNumber)
+            cout << "Too high! Try again.\n\n";
+
     } while (guess != secretNumber);
+
 
     return 0;
 }
