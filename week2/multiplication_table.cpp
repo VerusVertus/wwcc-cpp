1aa20a63fb41e4dfddfa0cc8b9451e9f5f1e71fa
Trevor Moses <mosest68@gmail.com>
2026-04-18 20:12:58 -0700

    Changing endl to \n. Laying out program 'map'

--- week2/multiplication_table.cpp
+++ week2/multiplication_table.cpp
@@ -1,5 +1,16 @@
 /*
-Author info
+Program:        Coin Flip Generator
+Date Created:   20260404
+Author:         Trevor Moses
+Purpose:        To complete Code Assignment 203.
+
+Requirements
+Ask the user for a starting number and an ending number
+Check that the starting number is less than the ending number
+Generate a multiplication table from the starting number to the ending number
+Format the table neatly with properly aligned columns
+Include row and column headers showing the numbers being multiplied
+
 */
 
 #include <iostream>
@@ -9,7 +20,7 @@ using namespace std;
 int main() {
     int start, end;
 
-    cout << "Multiplication Table Generator" << endl << endl;
+    cout << "Multiplication Table Generator\n\n";
 
     cout << "Enter starting number: ";
     cin >> start;
@@ -18,13 +29,21 @@ int main() {
     cin >> end;
 
     if (start > end) {
-        cout << "Error: Starting number must be less than ending number." << endl;
+        cout << "Error: Starting number must be less than ending number.\n";
         return 1;
     }
 
-    cout << endl;
+    cout << "\n";
 
-    // Your code here
+    //Need to deterimne size for max table size.
+
+
+    //Print X values
+
+
+    //Print Y values
+
+    //fill table
 
     return 0;
 }
