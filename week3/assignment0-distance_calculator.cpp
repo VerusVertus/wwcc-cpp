a5067368cc6f9655a4d51c8988590fc0e71d8a4a
Trevor Moses <mosest68@gmail.com>
2026-04-19 14:51:17 -0700

    Adding week3 folder. Created first assignment.

---
+++ week3/assignment0-distance_calculator.cpp
@@ -0,0 +1,42 @@
+/*
+Program:        distance calculator
+Date Created:   20260419
+Author:         Trevor Moses
+Purpose:        To complete Code Assignment 300.
+*/
+
+#include <iostream>
+#include <cmath>     // For sqrt()
+#include <iomanip>   // For setprecision() and fixed
+
+using namespace std;
+
+
+//function 1 - calculateDistance
+
+
+
+//funtion 2 - calculateMidpoint
+
+
+
+int main() {
+
+    double x1, y1, x2, y2;
+
+    //user Prompt for first set of coordinates (X)
+
+
+    //user prompt ofr second set (Y)
+
+
+
+    //Calculate distances section (midpoint and distance). This is where we should invoke the functions.
+
+    //distance = √[(x₂ - x₁)² + (y₂ - y₁)²]
+
+    //midpoint = ((x₁+x₂)/2, (y₁+y₂)/2)
+
+    //Display the coordinates entered, then display the midpoint.
+
+}
