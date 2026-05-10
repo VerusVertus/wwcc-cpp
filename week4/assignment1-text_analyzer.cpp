2504f93c90fa61de49d40fd7ff1e6bcbad3dc817
Trevor Moses <mosest68@gmail.com>
2026-05-10 13:06:31 -0700

    Added program layout

--- week4/assignment1-text_analyzer.cpp
+++ week4/assignment1-text_analyzer.cpp
@@ -5,3 +5,16 @@ Author:         Trevor Moses
 Purpose:        Practice string operations by building a tool that processes and reports on a block of text 
 */
 
+//count characters including spaces
+
+//Would number of words (break by spaces)
+
+//count sentences (count by . ? ! that are followed by a space?)
+
+//show longest word
+
+//show average word length
+
+//shows entire sentence in all uppercase
+
+//shows entire sentence in lower case
