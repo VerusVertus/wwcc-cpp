21762dc13a47ffc16955c736b67baeca25058e92
Trevor Moses <mosest68@gmail.com>
2026-05-09 17:49:13 -0700

    Adding week4 and starting work

---
+++ week4/assignment0-quiz_grade_tracker.cpp
@@ -0,0 +1,53 @@
+/*
+Program:        Quiz Grade Tracker
+Date Created:   20260509
+Author:         Trevor Moses
+Purpose:        To take in quiz scores then provide statistics for the group of grade.
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Define global number of quizes. Given to us by professor.
+const int NUM_QUIZZES = 5;
+
+//function for letter grade. 
+char getLetterGrade(double score)
+
+
+int main(){
+
+//declare the array for storage
+double array[];
+
+//prompt for scores
+
+
+//section for calculating stats
+
+//use a loop
+
+
+
+
+//Calculate letter grade. use helper function above
+
+
+
+//make display section
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} //End of main
